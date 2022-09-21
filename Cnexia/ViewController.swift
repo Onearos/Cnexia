5e12d0c1d63d3ed69242b8f17db62bf93c646ea7
XOniros <abdelmoneim.benaicha@gmail.com>
2022-09-21 15:30:40 +0100

    Initial Commit

---
+++ Cnexia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cnexia
+//
+//  Created by Macbook PRO on 21/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CnexiaUITests/CnexiaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CnexiaUITestsLaunchTests.swift
+//  CnexiaUITests
+//
+//  Created by Macbook PRO on 21/09/2022.
+//
+
+import XCTest
+
+class CnexiaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
